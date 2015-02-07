846c4ce17f15a1b033dd18f0cf8118f78ca03ab6
vagrant <vagrant@precise32.(none)>
2015-02-07 16:25:27 +0000

    added play and pause buttons to the album view

--- app/scripts/album.js
+++ app/scripts/album.js
@@ -28,16 +28,78 @@ var albumPicasso = {
      ]
   };
 
+  var currentlyPlayingSong = null;
+
  var createSongRow = function(songNumber, songName, songLength) {
    var template =
        '<tr>'
-     + '  <td class="col-md-1">' + songNumber + '</td>'
+       // this row is basically storing the song number as a html data attribute that JS can read from later.
+       // the attribute is called song-number (basically whatever is after the 'data-')
+     + '  <td class="song-number col-md-1" data-song-number="' + songNumber + '">' + songNumber + '</td>'
      + '  <td class="col-md-9">' + songName + '</td>'
      + '  <td class="col-md-2">' + songLength + '</td>'
      + '</tr>'
      ;
- 
-   return $(template);
+  
+  // instead of returning the row immediately, we'll attach hover functionality first
+  var $row = $(template);
+
+  var onHover = function(event) {
+    var songNumberCell = $(this).find('.song-number');
+    // get the song-number data from the div html
+    var songNumber = songNumberCell.data('song-number');
+    
+    // create the conditional to show play button if the user is hovering on a song that isn't the one playing
+    if (currentlyPlayingSong !== songNumber) {
+    // this line sets the play button on hover
+    songNumberCell.html('<a class="album-song-button"><i class="fa fa-play"></i></a>');
+    }
+  };
+
+  var offHover = function(event) {
+    var songNumberCell = $(this).find('.song-number');
+    var songNumber = songNumberCell.data('song-number');
+
+    if (songNumber !== currentlyPlayingSong) {
+    songNumberCell.html(songNumber);
+    }
+  };
+
+  // toggle the play, pause and song number based on the button clicked
+  var clickHandler = function(event) {
+    var songNumber = $(this).data('song-number')
+
+    // if a song is playing and is clicked, handle what happens with the UI but don't do anything else
+    if (currentlyPlayingSong !== null) {
+      // stop playing the current song.
+      // replace stopped song button with number
+      // set the data attribute on .song-number div to that of the one that just got clicked
+      currentlyPlayingCell = $('.song-number[data-song-number="' + currentlyPlayingSong + '"]');
+      currentlyPlayingCell.html(currentlyPlayingSong);
+    }
+
+    // a non playing song was clicked
+    if (currentlyPlayingSong !== songNumber) {
+      // a play icon wil be showing because of hover
+      // Switch from Play -> Pause to indicate new song is playing
+      $(this).html('<a class="album-song-button"><i class="fa fa-pause"></i></a>');
+
+      // Set the current song to the one clicked
+      currentlyPlayingSong = songNumber;
+
+    } else if ( currentlyPlayingSong === songNumber ) {
+      // switch from Pause -> Play to indicate that song was paused
+      $(this).html('<a class="album-song-button"><i class="fa fa-play"></i></a>');
+
+      // set the current song to null
+      currentlyPlayingSong = null;
+    }
+  };
+
+  $row.find('.song-number').click(clickHandler);
+  $row.hover(onHover, offHover);
+  return $row;
+
  };
  
 var changeAlbumView = function(album) {
