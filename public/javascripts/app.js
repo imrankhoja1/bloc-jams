fe0d5a65c7d545a7108cb34157f0ac30627c90e1
vagrant <vagrant@precise32.(none)>
2015-02-17 13:07:42 +0000

    fix backgorund color issue on profile

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -284,21 +284,32 @@ angular.module('BlocJams', []).controller('Landing.controller', ['$scope', funct
   $scope.title = "Bloc Jams";
   $scope.subText = "Turn the music up!";
 
+  function shuffle(o){ //v1.0
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+  };
+
   $scope.subTextClicked = function(event) {
     $scope.subText += '!';
   };
 
-  $scope.albumURLs = [
-  '/images/album-placeholders/album-1.jpg',
-  '/images/album-placeholders/album-2.jpg',
-  '/images/album-placeholders/album-3.jpg',
-  '/images/album-placeholders/album-4.jpg',
-  '/images/album-placeholders/album-5.jpg',
-  '/images/album-placeholders/album-6.jpg',
-  '/images/album-placeholders/album-7.jpg',
-  '/images/album-placeholders/album-8.jpg',
-  '/images/album-placeholders/album-9.jpg',
-];
+  $scope.titleClicked = function(event) {
+    $scope.albumURLs = shuffle(albumURLs);
+  };
+
+  var albumURLs = [
+    '/images/album-placeholders/album-1.jpg',
+    '/images/album-placeholders/album-2.jpg',
+    '/images/album-placeholders/album-3.jpg',
+    '/images/album-placeholders/album-4.jpg',
+    '/images/album-placeholders/album-5.jpg',
+    '/images/album-placeholders/album-6.jpg',
+    '/images/album-placeholders/album-7.jpg',
+    '/images/album-placeholders/album-8.jpg',
+    '/images/album-placeholders/album-9.jpg',
+  ];
+
+  $scope.albumURLs = albumURLs;
 }]);
 });
 
