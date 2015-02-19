f6aaae0573205a5032c12ef477706d011b993f64
vagrant <vagrant@precise32.(none)>
2015-02-19 12:58:45 +0000

    Converted collection view to angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,6 +3,21 @@
 // require("./album");
 // require("./profile");
 
+var albumPicasso = {
+  name: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtUrl: '/images/album-placeholder.png',
+  songs: [
+      { name: 'Blue', length: '4:26' },
+      { name: 'Green', length: '3:14' },
+      { name: 'Red', length: '5:01' },
+      { name: 'Pink', length: '3:21' },
+      { name: 'Magenta', length: '2:15' }
+    ]
+};
+
 blocJams = angular.module('BlocJams', ['ui.router']);
 blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
   // this just means we want our states to match plain routes (read single page arch checkpoint) and not have routes prefixed with /#!/
@@ -17,6 +32,12 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
   $stateProvider.state('song', {
     url: '/song',
     templateUrl: '/templates/song.html'
+  });
+
+  $stateProvider.state('collection', {
+    url: '/collection',
+    controller: 'Collection.controller',
+    templateUrl: '/templates/collection.html'
   });
 }]);
 
@@ -50,4 +71,11 @@ blocJams.controller('Landing.controller', ['$scope', function($scope) {
   ];
 
   $scope.albumURLs = albumURLs;
+}]);
+
+blocJams.controller('Collection.controller', ['$scope', function($scope) {
+  $scope.albums = [];
+  for (var i = 0; i < 33; i++) {
+    $scope.albums.push(angular.copy(albumPicasso));
+  }
 }]);
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -280,6 +280,21 @@ var setupSeekBars = function () {
 // require("./album");
 // require("./profile");
 
+var albumPicasso = {
+  name: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtUrl: '/images/album-placeholder.png',
+  songs: [
+      { name: 'Blue', length: '4:26' },
+      { name: 'Green', length: '3:14' },
+      { name: 'Red', length: '5:01' },
+      { name: 'Pink', length: '3:21' },
+      { name: 'Magenta', length: '2:15' }
+    ]
+};
+
 blocJams = angular.module('BlocJams', ['ui.router']);
 blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
   // this just means we want our states to match plain routes (read single page arch checkpoint) and not have routes prefixed with /#!/
@@ -294,6 +309,12 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
   $stateProvider.state('song', {
     url: '/song',
     templateUrl: '/templates/song.html'
+  });
+
+  $stateProvider.state('collection', {
+    url: '/collection',
+    controller: 'Collection.controller',
+    templateUrl: '/templates/collection.html'
   });
 }]);
 
@@ -327,6 +348,13 @@ blocJams.controller('Landing.controller', ['$scope', function($scope) {
   ];
 
   $scope.albumURLs = albumURLs;
+}]);
+
+blocJams.controller('Collection.controller', ['$scope', function($scope) {
+  $scope.albums = [];
+  for (var i = 0; i < 33; i++) {
+    $scope.albums.push(angular.copy(albumPicasso));
+  }
 }]);
 });
 
